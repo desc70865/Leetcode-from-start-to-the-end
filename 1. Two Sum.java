d59cfcdec52fdd4fef2bb916dc2200f006039ae4
desc_inno <17777904+desc70865@users.noreply.github.com>
2020-05-18 11:24:45 +0800

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -1,3 +1,16 @@
+/* 
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
+ 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -11,4 +24,5 @@ class Solution {
         throw new IllegalArgumentException("No two sum solution");
     }
 }
+
 // 利用 hash 表(牺牲内存换取的)索引存储优势,同时进行查找和插入操作的循环
