f3ae07cdb76a951d4e2597f6c94fd6ab1c656bd5
desc_inno <17777904+desc70865@users.noreply.github.com>
2020-05-21 08:55:25 +0800

    Update 0006. ZigZag Conversion.java

--- 0~100/0006. ZigZag Conversion.java
+++ 0~100/0006. ZigZag Conversion.java
@@ -13,6 +13,7 @@ Example 1:
 
 Input: s = "PAYPALISHIRING", numRows = 3
 Output: "PAHNAPLSIIGYIR"
+
 Example 2:
 
 Input: s = "PAYPALISHIRING", numRows = 4
@@ -46,6 +47,5 @@ P     I
 }
 
 // 当然在Z字两端判断 step *= -1 也是简洁的写法
-// 
 // 可惜输出不含空格,不然会有趣很多
 // 可以先求转置数组再转置输出,大致可节省一半的插入时间
