9f5fd5d55e5515f037ccec07302c7ad024ab6476
desc70865 <descinno@gmail.com>
2020-07-14 21:47:19 +0800

    update

---
+++ 1300~1400/1344. Angle Between Hands of a Clock.java
@@ -0,0 +1,48 @@
+/* 
+Given two numbers, hour and minutes. Return the smaller angle (in degrees) formed between the hour and the minute hand.
+
+ 
+
+Example 1:
+
+
+
+Input: hour = 12, minutes = 30
+Output: 165
+Example 2:
+
+
+
+Input: hour = 3, minutes = 30
+Output: 75
+Example 3:
+
+
+
+Input: hour = 3, minutes = 15
+Output: 7.5
+Example 4:
+
+Input: hour = 4, minutes = 50
+Output: 155
+Example 5:
+
+Input: hour = 12, minutes = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= hour <= 12
+0 <= minutes <= 59
+Answers within 10^-5 of the actual value will be accepted as correct.
+ */
+
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double min = minutes * 6;
+        double h = hour % 12 * 30 + min / 12;
+        double res = Math.abs(h - min);
+        return res > 180 ? 360 - res : res;
+    }
+}
