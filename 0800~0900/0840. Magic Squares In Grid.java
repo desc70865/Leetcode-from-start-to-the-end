3715476bf3c83b09bf2848dcbd5131025ce3019a
desc_inno <descinno@gmail.com>
2021-02-03 17:22:10 +0800

    update

---
+++ 0800~0900/0840. Magic Squares In Grid.java
@@ -0,0 +1,74 @@
+/* 
+A 3 x 3 magic square is a 3 x 3 grid filled with distinct numbers from 1 to 9 such that each row, column, and both diagonals all have the same sum.
+
+Given a row x col grid of integers, how many 3 x 3 "magic square" subgrids are there?  (Each subgrid is contiguous).
+
+ 
+
+Example 1:
+
+
+Input: grid = [[4,3,8,4],[9,5,1,9],[2,7,6,2]]
+Output: 1
+Explanation: 
+The following subgrid is a 3 x 3 magic square:
+
+while this one is not:
+
+In total, there is only one magic square inside the given grid.
+Example 2:
+
+Input: grid = [[8]]
+Output: 0
+Example 3:
+
+Input: grid = [[4,4],[3,3]]
+Output: 0
+Example 4:
+
+Input: grid = [[4,7,8],[9,5,1],[2,3,6]]
+Output: 0
+ 
+
+Constraints:
+
+row == grid.length
+col == grid[i].length
+1 <= row, col <= 10
+0 <= grid[i][j] <= 15
+ */
+
+class Solution {
+    public int numMagicSquaresInside(int[][] grid) {
+        int R = grid.length, C = grid[0].length;
+        int ans = 0;
+        for (int r = 0; r < R - 2; r++)
+            for (int c = 0; c < C - 2; c++) {
+                if (grid[r+1][c+1] != 5) continue;
+                if (magic(grid[r][c], grid[r][c+1], grid[r][c+2],
+                          grid[r+1][c], grid[r+1][c+1], grid[r+1][c+2],
+                          grid[r+2][c], grid[r+2][c+1], grid[r+2][c+2]))
+                    ans++;
+            }
+        return ans;
+    }
+
+    public boolean magic(int... vals) {
+        int[] map = new int[16];
+        for (int v: vals) {
+            map[v]++;
+        }
+        for (int v = 1; v <= 9; v++)
+            if (map[v] != 1)
+                return false;
+
+        return (vals[0] + vals[1] + vals[2] == 15 &&
+                vals[3] + vals[4] + vals[5] == 15 &&
+                vals[6] + vals[7] + vals[8] == 15 &&
+                vals[0] + vals[3] + vals[6] == 15 &&
+                vals[1] + vals[4] + vals[7] == 15 &&
+                vals[2] + vals[5] + vals[8] == 15 &&
+                vals[0] + vals[4] + vals[8] == 15 &&
+                vals[2] + vals[4] + vals[6] == 15);
+    }
+}
