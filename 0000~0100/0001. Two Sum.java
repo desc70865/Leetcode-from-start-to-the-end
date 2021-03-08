e3360d443df6836fe14058fa97f6750a01d50478
desc_inno <descinno@gmail.com>
2021-03-08 22:15:57 +0800

    update

--- 0000~0100/0001. Two Sum.java
+++ 0000~0100/0001. Two Sum.java
@@ -29,44 +29,7 @@ class Solution {
     }
 }
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int LEN = nums.length;
-        int[] arr = Arrays.copyOf(nums, LEN);
-        Arrays.sort(nums);
-        int i = 0, j = LEN - 1;
-        while (i < j) {
-            int sum = nums[i] + nums[j];
-            if (sum < target) {
-                i++;
-            } else if (sum > target) {
-                j--;
-            } else {
-                break;
-            }
-        }
-        int t = i;
-        for (int k = 0; k < LEN; k++) {
-            if (arr[k] == nums[i]) {
-                i = k;
-                break;
-            }
-        }
-        /* for (int k = 0; k < LEN; k++) {
-            if (arr[k] == nums[j] && k != i) {
-                j = k;
-                break;
-            }
-        } */
-        for (int k = (t == j) ? i + 1 : 0; k < LEN; k++) {
-            if (arr[k] == nums[j]) {
-                j = k;
-                break;
-            }
-        }
-        return new int[] {i, j};
-    }
-}
+
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -82,63 +45,25 @@ class Solution {
     }
 }
 
+
+
 class Solution {
+    static final long HALF = 2147483648L;
+    static final long OFFSET = 10;
+
     public int[] twoSum(int[] nums, int target) {
-        int LEN = nums.length;
-        int[][] arr = new int[LEN][2];
-        for (int i = 0; i < LEN; i++) {
-            arr[i][0] = nums[i];
-            arr[i][1] = i;
+        int len = nums.length;
+        long[] arr = new long[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = ((long) nums[i] + HALF << OFFSET) + i;
         }
-        Arrays.sort(arr, (x, y) -> (x[0] - y[0]));
-        // Arrays.sort(arr, new Comparator<int[]>() {
-        //     @Override
-        //     public int compare(int[] x, int[] y) {
-        //         return x[0] - y[0];
-        //     }
-        // });
-        // sort(arr, 0, LEN - 1);
-        int i = 0, j = LEN - 1;
-        while (i < j) {
-            int sum = arr[i][0] + arr[j][0];
-            if (sum < target) i++;
-            else if (sum > target) j--;
-            else return new int[] { arr[i][1], arr[j][1] };
+        Arrays.sort(arr);
+        for (int l = 0, r = len - 1; l < r;) {
+            long d = (arr[l] >> OFFSET) + (arr[r] >> OFFSET) - 4294967296L - target;
+            if (d == 0) return new int[] {(int) (arr[l] % 1024), (int) (arr[r] % 1024)};
+            else if (d < 0) l++;
+            else r--;
         }
-        throw new IllegalArgumentException("No two sum solution");
+        return new int[0];
     }
-    
-    /* private void sort(int[][] arr, int l, int r) {
-        int LEN = r - l;
-        if (LEN < 0) {
-            return;
-        }
-        int pivot = partition(arr, l, r);
-        sort(arr, l, pivot-1);
-        sort(arr, pivot+1, r);
-    }
-    
-    private int partition(int[][] arr, int l, int r) {
-        int pivot = arr[l][0], index = arr[l][1];
-        while (l < r) {
-            while (l < r) {
-                if (arr[r][0] < pivot) {
-                    arr[l][0] = arr[r][0];
-                    arr[l][1] = arr[r][1];
-                    break;
-                }
-                r--;
-            }
-            while (l < r) {
-                if (arr[l][0] > pivot) {
-                    arr[r][0] = arr[l][0];
-                    arr[r][1] = arr[l][1];
-                    break;
-                }
-                l++;
-            }
-        }
-        arr[l][0] = pivot; arr[l][1] = index;
-        return l;
-    } */
 }
