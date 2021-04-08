514d6fb0e760101608db9ef520d767a0b6c35ec5
desc_inno <descinno@gmail.com>
2021-04-09 06:52:54 +0800

    update

--- 0100~0200/0154. Find Minimum in Rotated Sorted Array II.java
+++ 0100~0200/0154. Find Minimum in Rotated Sorted Array II.java
@@ -24,22 +24,7 @@ Would allow duplicates affect the run-time complexity? How and why?
 
 class Solution {
     public int findMin(int[] nums) {
-        int len = nums.length;
-        if (len == 0) return -1;
-        else if (len == 1) return nums[0];
-        if (nums[0] < nums[len-1]) return nums[0];
-        for (int i=1; i < len; i++) {
-            if (nums[i] < nums[i-1]) return nums[i];
-        }
-        return nums[0];
-    }
-}
-
-
-class Solution {
-    public int findMin(int[] nums) {
-        int low = 0, high = nums.length-1;
-
+        int low = 0, high = nums.length - 1;
         while (low < high) {
             int pivot = low + (high - low) / 2;
             if (nums[pivot] < nums[high])
@@ -47,30 +32,8 @@ class Solution {
             else if (nums[pivot] > nums[high])
                 low = pivot + 1;
             else
-                high -= 1;
+                high--;
         }
         return nums[low];
-    }
-}
-
-
-
-class Solution {
-    public int findMin(int[] nums) {
-        return biSearch(nums, 0, nums.length-1);
-    }
-
-    private int biSearch(int[] nums, int l, int r) {
-        if (l == r) {
-            return nums[l];
-        }
-        int mid = (l + r) / 2;
-        if (nums[mid] < nums[r]) {
-            return biSearch(nums, l, mid);
-        } else if (nums[mid] > nums[r]) {
-            return biSearch(nums, mid + 1, r);
-        } else {
-            return biSearch(nums, l, r - 1);
-        }
     }
 }
