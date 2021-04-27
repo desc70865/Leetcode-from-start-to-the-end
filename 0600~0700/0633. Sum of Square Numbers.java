43ffd24e89e637b086fc638d99abc4411ba71df9
desc_inno <descinno@gmail.com>
2021-04-28 06:45:20 +0800

    update

--- 0600~0700/0633. Sum of Square Numbers.java
+++ 0600~0700/0633. Sum of Square Numbers.java
@@ -16,14 +16,6 @@ Output: False
 
 class Solution {
     public boolean judgeSquareSum(int c) {
-        
-    }
-}
-
-
-
-class Solution {
-    public boolean judgeSquareSum(int c) {
         for (long a = 0; a * a <= c; a++) {
             double b = Math.sqrt(c - a * a);
             if (b == (int) b) return true;
@@ -36,15 +28,14 @@ class Solution {
 
 class Solution {
     public boolean judgeSquareSum(int c) {
-        for (int i = 2; i * i <= c; i++) {
-            int count = 0;
-            if (c % i == 0) {
-                while (c % i == 0) {
-                    count++;
-                    c /= i;
-                }
-                if (i % 4 == 3 && count % 2 != 0) return false;
+        for (int factor = 2; factor * factor <= c; factor++) {
+            if (c % factor != 0) continue;
+            int cnt = 0;
+            while (c % factor == 0) {
+                cnt++;
+                c /= factor;
             }
+            if (factor % 4 == 3 && cnt % 2 != 0) return false;
         }
         return c % 4 != 3;
     }
