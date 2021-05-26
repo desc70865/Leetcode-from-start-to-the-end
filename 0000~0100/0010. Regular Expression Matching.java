72843a9f61885f8d6eb7e64ed2e44f4af04b7fbf
desc_inno <descinno@gmail.com>
2021-05-26 19:37:22 +0800

    update

--- 0000~0100/0010. Regular Expression Matching.java
+++ 0000~0100/0010. Regular Expression Matching.java
@@ -52,83 +52,22 @@ Output: false
 
 class Solution {
     public boolean isMatch(String text, String pattern) {
-        if (pattern.isEmpty()) return text.isEmpty();
-        boolean first_match = (!text.isEmpty() &&
-                               (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
-
-        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
-            return (isMatch(text, pattern.substring(2)) ||
-                    (first_match && isMatch(text.substring(1), pattern)));
-        } else {
-            return first_match && isMatch(text.substring(1), pattern.substring(1));
-        }
-    }
-}
-
-// 显式递归
-// *分割,迭代子串
-
-enum Result {
-    TRUE, FALSE
-}
-
-class Solution {
-    Result[][] memo; // boolean 2-d array
-
-    public boolean isMatch(String text, String pattern) {
-        memo = new Result[text.length() + 1][pattern.length() + 1];
-        return dp(0, 0, text, pattern);
-    }
-
-    public boolean dp(int i, int j, String text, String pattern) {
-        if (memo[i][j] != null) {
-            return memo[i][j] == Result.TRUE;
-        }
-        boolean ans;
-        if (j == pattern.length()){
-            ans = i == text.length();
-        } else{
-            boolean first_match = (i < text.length() &&
-                                   (pattern.charAt(j) == text.charAt(i) ||
-                                    pattern.charAt(j) == '.'));
-
-            if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-                ans = (dp(i, j+2, text, pattern) ||
-                       first_match && dp(i+1, j, text, pattern));
-            } else {
-                ans = first_match && dp(i+1, j+1, text, pattern);
-            }
-        }
-        memo[i][j] = ans ? Result.TRUE : Result.FALSE;
-        return ans;
-    }
-}
-
-// 自顶向下,动态规划
-
-class Solution {
-    public boolean isMatch(String text, String pattern) {
-        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
-        dp[text.length()][pattern.length()] = true;
-
-        for (int i = text.length(); i >= 0; i--){
-            for (int j = pattern.length() - 1; j >= 0; j--){
-                boolean first_match = (i < text.length() &&
-                                       (pattern.charAt(j) == text.charAt(i) ||
-                                        pattern.charAt(j) == '.'));
-                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+        char[] s = text.toCharArray();
+        char[] t = pattern.toCharArray();
+        int m = s.length;
+        int n = t.length;
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[m][n] = true;
+        for (int i = m; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                boolean f = i < m && (s[i] == t[j] || '.' == t[j]);
+                if (j + 1 < n && t[j + 1] == '*') {
+                    dp[i][j] = dp[i][j + 2] || f && dp[i + 1][j];
                 } else {
-                    dp[i][j] = first_match && dp[i+1][j+1];
+                    dp[i][j] = f && dp[i + 1][j + 1];
                 }
             }
         }
         return dp[0][0];
     }
 }
-
-// 自底向上,动态规划
-// 正则匹配判断: 在字符串合法长度内等于原字符或'.'
-// 在循环体中,若后置'*',则字符串右移或正则右移两位
-// 问题是这里为什么用或: 应对 * == 0 的情况,这是正则自身规则决定的
-// 否则直接匹配并同时右移
