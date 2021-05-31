434c8690532f3fea5f50d363c599a473db59c47b
desc_inno <descinno@gmail.com>
2021-06-01 07:19:11 +0800

    update

--- 1700~1800/1744. Can You Eat Your Favorite Candy on Your Favorite Day.java
+++ 1700~1800/1744. Can You Eat Your Favorite Candy on Your Favorite Day.java
@@ -41,22 +41,21 @@ queries[i].length == 3
 
 class Solution {
     public boolean[] canEat(int[] candiesCount, int[][] queries) {
-        int size = candiesCount.length;
-        long[] candy = new long[size];
-        candy[0] = candiesCount[0];
-        for (int i = 1; i < size; i++) {
-            candy[i] = candiesCount[i] + candy[i - 1];
+        int n = candiesCount.length;
+        long[] candy = new long[n + 1];
+        for (int i = 1; i <= n; i++) {
+            candy[i] = candy[i - 1] + candiesCount[i - 1];
         }
-        int len = queries.length;
-        boolean[] ans = new boolean[len];
-        for (int i = 0; i < len; i++) {
-            ans[i] = helper(candy, queries[i]);
+        boolean[] ans = new boolean[queries.length];
+        int p = 0;
+        for (int[] q: queries) {
+            ans[p] = helper(candy, q[0] + 1, q[1] + 1, q[2]);
+            ++p;
         }
         return ans;
     }
     
-    private boolean helper(long[] candy, int[] A) {
-        return ! (A[0] > 0 && candy[A[0] - 1] >= (long) A[2] * (A[1] + 1)
-                || candy[A[0]] < A[1] + 1);
+    private boolean helper(long[] c, int n, int t, int k) {
+        return t <= c[n] && c[n - 1] < (long) k * t;
     }
 }
