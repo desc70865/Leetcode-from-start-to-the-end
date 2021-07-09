5b88f89afa13e00d5d3f00e99908148edc0167f1
desc_inno <descinno@gmail.com>
2021-07-09 19:38:27 +0800

    update

--- 0300~0400/0347. Top K Frequent Elements.java
+++ 0300~0400/0347. Top K Frequent Elements.java
@@ -20,11 +20,48 @@ You can return the answer in any order.
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int num: nums) map.merge(num, 1, Integer::sum);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        for (int key: map.keySet()) pq.offer(new int[] {key, map.get(key)});
+        for (int num: nums) {
+            map.merge(num, 1, Integer::sum);
+        }
+        int[][] bucket = new int[map.size()][2];
+        int idx = 0;
+        for (int key: map.keySet()) {
+            bucket[idx][0] = key;
+            bucket[idx][1] = map.get(key);
+            ++idx;
+        }
+        quickSelect(bucket, 0, bucket.length - 1, k);
         int[] ans = new int[k];
-        for (int i = 0; i < k; i++) ans[i] = pq.poll()[0];
+        for (int i = 0; i < k; ++i) {
+            ans[i] = bucket[i][0];
+        }
         return ans;
+    }
+
+    private int quickSelect(int[][] arr, int low, int high, int k) {
+        int partition = partition(arr, low, high);
+        if (partition == k - 1) {
+            return arr[partition][1];
+        } else if (partition < k - 1) {
+            return quickSelect(arr, partition + 1, high, k);
+        } else {
+            return quickSelect(arr, low, partition - 1, k);
+        }
+    }
+
+    private int partition(int[][] arr, int low, int high) {
+        int pivot = arr[low][1], pivotIdx = high;
+        for (int i = high; i >= low; --i) {
+            if (arr[i][1] < pivot) {
+                int[] tmp = arr[i];
+                arr[i] = arr[pivotIdx];
+                arr[pivotIdx] = tmp;
+                --pivotIdx;
+            }
+        }
+        int[] tmp = arr[low];
+        arr[low] = arr[pivotIdx];
+        arr[pivotIdx] = tmp;
+        return pivotIdx;
     }
 }
