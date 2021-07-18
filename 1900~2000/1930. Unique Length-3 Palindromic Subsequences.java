d642a3330f0d19dbfc2c33e64703266c63d43b52
desc70865 <descinno@gmail.com>
2021-07-18 12:41:19 +0800

    update

---
+++ 1900~2000/1930. Unique Length-3 Palindromic Subsequences.java
@@ -0,0 +1,66 @@
+/* 
+Given a string s, return the number of unique palindromes of length three that are a subsequence of s.
+
+Note that even if there are multiple ways to obtain the same subsequence, it is still only counted once.
+
+A palindrome is a string that reads the same forwards and backwards.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+ 
+
+Example 1:
+
+Input: s = "aabca"
+Output: 3
+Explanation: The 3 palindromic subsequences of length 3 are:
+- "aba" (subsequence of "aabca")
+- "aaa" (subsequence of "aabca")
+- "aca" (subsequence of "aabca")
+Example 2:
+
+Input: s = "adc"
+Output: 0
+Explanation: There are no palindromic subsequences of length 3 in "adc".
+Example 3:
+
+Input: s = "bbcbaba"
+Output: 4
+Explanation: The 4 palindromic subsequences of length 3 are:
+- "bbb" (subsequence of "bbcbaba")
+- "bcb" (subsequence of "bbcbaba")
+- "bab" (subsequence of "bbcbaba")
+- "aba" (subsequence of "bbcbaba")
+ 
+
+Constraints:
+
+3 <= s.length <= 105
+s consists of only lowercase English letters.
+ */
+
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        char[] t = s.toCharArray();
+        int n = t.length;
+        int ans = 0;
+        for (char c = 'a'; c <= 'z'; ++c) {
+            int l = s.indexOf(c);
+            int r = s.lastIndexOf(c);
+            if (l + 1 >= r) {
+                continue;
+            }
+            int[] map = new int[26];
+            for (int k = l + 1; k < r; ++k) {
+                ++map[t[k] - 'a'];
+            }
+            for (int i = 0; i < 26; ++i) {
+                if (map[i] > 0) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
